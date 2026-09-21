886f95cf5a09feb64bbc5dd09b31db9c0969f984
Bielsampaio25 <gabrielsampaio009officer@gmail.com>
2026-09-21 17:36:48 -0300

    criação da tela de cadastro e login.

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-cadastro',
   imports: [RouterLink],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.css',
 })
-export class Home {
+export class Cadastro {
 
 }
