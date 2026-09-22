1a78f8b005e1e6f8b8475bd250cb82df6af23973
Bielsampaio25 <gabrielsampaio009officer@gmail.com>
2026-09-21 21:22:40 -0300

    começo da edição da página site.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Cadastro } from './pages/cadastro/cadastro';
-
+import { Site } from './pages/site/site';
 export const routes: Routes = [
     {path:"", redirectTo:"home", pathMatch:"full"},
     {path:"home", component:Home},
     {path:"login", component:Login},
-    {path:"cadastro", component:Cadastro}
+    {path:"cadastro", component:Cadastro},
+    {path:"site", component:Site}
 ];
---
+++ src/app/pages/site/site.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from '../../componentes/sidebar/sidebar';
+
+@Component({
+  selector: 'app-site',
+  imports: [Sidebar],
+  templateUrl: './site.html',
+  styleUrl: './site.css',
+})
+export class Site {
+
+}
